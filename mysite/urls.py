6f55b454129489f6c20acdf0bce2c75a6bc53950
Ivan Cabellos <icabellos@apsl.net>
2019-05-10 08:52:02 +0200

    10/05/2019 Ivan Cabellos. Añadido temas de seguridad a la página, tienes que estar loggeado para poder añadir/editar posts

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
 ]
